69dcbb1700f6ed8b6295e80588369eae446936b3
Ella Young <ellyoung@publicisgroupe.net>
2024-07-01 13:50:51 -0400

    Removing default styles

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,7 @@ import SearchBar from './components/SearchBar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Crochet Compass</h1>
-        <SearchBar />
-      </header>
+      <SearchBar/>
     </div>
   );
 }
