645562c414067df5d002e4283046761985072872
Ella Young <ellasyoung@gmail.com>
2024-07-20 21:30:53 -0400

    Configuring CORS

--- functions/index.js
+++ functions/index.js
@@ -2,24 +2,26 @@ const functions = require('firebase-functions');
 const express = require('express');
 const cors = require('cors');
 
-// Initialize Express app
 const app = express();
 
-// Middleware
 app.use(cors({
-    origin: 'https://crochetcompass.web.app/explore' 
+    origin: 'https://crochetcompass.web.app' 
 }));
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Define routes
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the CrochetCompass API' });
 });
 
-// Import and use your routes
 const patternsRouter = require('./routes/patterns');
 app.use('/api/patterns', patternsRouter);
 
-// Export the Express app as a Firebase Function
 exports.api = functions.https.onRequest(app);
--- server.js
+++ server.js
@@ -11,6 +11,16 @@ connectDB();
 const app = express();  
 
 // Middleware
+app.use(cors({
+    origin: 'https://crochetcompass.web.app' 
+}));
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(cors());
 app.use(express.json());
 
